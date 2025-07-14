abf1842a2c8f5cb51632231efcae062cf2c3e17f
usurana <usurana@fortinet.com>
2025-07-14 09:46:31 +0530

    Added support for DSN less connection

--- odbc/operations.py
+++ odbc/operations.py
@@ -6,15 +6,40 @@ Copyright end
 """
 
 from connectors.core.connector import get_logger, ConnectorError
-import json
 import pyodbc
 
 logger = get_logger('odbc')
 
 
 def connect_to_db(config):
+    if config.get("connection_type") == "Using DSN":
+        conn_str = f"DSN={config.get('dsn_name')};"
+    else:
+        conn_items = [
+            ("Driver", config.get("driver")),
+            ("Host", config.get("host")),
+            ("Port", config.get("port")),
+            ("UID", config.get("username")),
+            ("PWD", config.get("token")),
+            ("HTTPPath", config.get("http_path")),
+            ("AuthMech", config.get("auth_mech")),
+            ("SSL", config.get("ssl")),
+            ("ThriftTransport", config.get("thrift_transport")),
+            ("Database", config.get("database"))
+        ]
+        conn_str_parts = []
+        for key, value in conn_items:
+            if value is not None and value != "":
+                if key == "Driver":
+                    conn_str_parts.append(f"{key}={{{value}}}")
+                else:
+                    conn_str_parts.append(f"{key}={value}")
+        conn_str = ";".join(conn_str_parts) + ";"
+        if config.get("extra_options"):
+            conn_str += config["extra_options"].strip().rstrip(";") + ";"
     try:
-        conn = pyodbc.connect(f"DSN={config.get('dsn_name')};")
+        logger.error("The connection String is -------> {}".format(conn_str))
+        conn = pyodbc.connect(conn_str)
         conn.autocommit = config.get("auto_commit")
         return conn
     except pyodbc.InterfaceError as ie:
